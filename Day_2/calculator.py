bb91cb57df76b42c3e527c8462959c02e32960e8
Saptarshi Majumdar <saptarshi.maj@yahoo.com>
2025-09-20 13:01:31 +0530

    Day_2 calculator program updated

--- Day_2/calculator.py
+++ Day_2/calculator.py
@@ -1,7 +1,18 @@
 def calculate():
   x = int(input("Enter first number: "))
   y = int(input("Enter second number: "))
-  
-  result = f"The result is: {x + y}"
-  print(result)
+  choice = int(input("Choose 1 to Add \n Choose 2 to Subtract \n Choose 3 to Multiply \n Choose 4 to Divide \n Choose 0 to exit \n"))
+  if choice == 1:
+    print(f"The sum is: {x + y}")
+  elif choice == 2:
+    print(f"The difference is: {x - y}")
+  elif choice == 3:
+    print(f"The product is: {x * y}")
+  elif choice == 4:
+    print(f"The result of division is: {x / y}") 
+  elif choice == 0:
+    print(f"Bye Bye, See you again soon")
+  else:
+    print("Please make a valid choice")
+    return
 calculate()
